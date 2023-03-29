b86f0c06edc64bff7b753b5d7d610c37910b0c91
DiegoDomingo <818637@unizar.es>
2023-03-29 16:55:48 +0200

    Versión que funciona
    Co-authored-by: Simon Alonso <821038@unizar.es>

--- separarPalabras.cc
+++ separarPalabras.cc
@@ -1,20 +1,45 @@
 #include <iostream>
-#include <fstream>
-#include <list>
+#include <string>
+#include <vector>
+
 using namespace std;
 
-string buscarPrefijo(string diccionario[12], string prefijo) {
-    for (int i = 0; i < 12; i++) {
-        if (diccionario[i] == prefijo) {
-            cout << "Prefijo: " << prefijo << " -> existe en " << diccionario[i] << endl;
-            return diccionario[i];
+bool buscarPrefijo(vector<string> diccionario, string prefijo) {
+    int n = diccionario.size();
+    for (int i = 0; i < n; i++) {
+        if (diccionario.at(i) == prefijo) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void descubrir_secuencia(vector<string> dict, string entrada, int i,vector<string> particion) {
+    //Si es el final de la cadena, imprimimos las particiones encontradas hasta ahora
+    if (i >= entrada.length()) { 
+        for (auto v : particion){
+            cout << v << " ,";
+        }
+        cout << endl;
+    }
+
+    //Recorrer todas las subcadenas desde el indice actual
+    for (int j = i+1; j <= entrada.length(); j++) { 
+        string prefijo = entrada.substr(i, j-i);
+        //Si encontramos prefijo, añadimo el prefijo a la particion
+        if (buscarPrefijo(dict,prefijo)) { 
+            particion.push_back(prefijo);
+            descubrir_secuencia(dict,entrada,j,particion);
+            //Si no se hace pop_back() se acumula el ultimo prefijo.
+            //Entonces en lugar de hacerlo bien hace: sol, soldar
+            particion.pop_back(); 
         }
     }
 }
 
-int main(int argc, char* argv[]) {
-    string diccionario[12] = {"me", "gusta", "sol", "dar", "soldar", "dinámico", "hielo", "nata", "helado", "lado", "do", "la"};
-    string entrada = "megustadolar";
-    list<string> palabras;
-    prueba(diccionario, entrada, 0, 0, palabras);
+int main() {
+    vector<string> diccionario= {"me", "gusta", "sol", "dar","d", "ar", "soldar", "soldar", "dinámico", "hielo", "nata", "helado", "lado", "do", "la"};
+    string entrada = "megustasoldar";
+    vector<string> particion;
+    descubrir_secuencia(diccionario, entrada, 0, particion);
 }
