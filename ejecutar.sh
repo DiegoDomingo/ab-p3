6082cef8fbd6fbbb05d583ce5f045e199fdb70fb
diegodom02 <818637@unizar.es>
2023-04-23 13:10:20 +0200

    Cambios script
    Co-authored-by: SimonAlonso <SagHack@users.noreply.github.com>

--- ejecutar.sh
+++ ejecutar.sh
@@ -15,14 +15,22 @@ g++ -std=c++11 separarPalabras.cc -o separarPalabras
 
 rm descifrado.txt 2>/dev/null
 
-echo "*********"
-echo "Prueba $i"
-echo "*********"
-
 # Ejecución de los programas
+echo "********"
+echo "Prueba 1"
+echo "********"
 ./separarPalabras dict100.txt salida100-1.txt 0
+echo "********"
+echo "Prueba 2"
+echo "********"
 ./separarPalabras dict100.txt salida100-2.txt 1
+echo "********"
+echo "Prueba 3"
+echo "********"
 ./separarPalabras dict1000.txt salida1000-1.txt 0
+echo "********"
+echo "Prueba 4"
+echo "********"
 ./separarPalabras dict1000.txt salida1000-2.txt 1
 
 # Limpieza de ejecutables y ficheros generados
